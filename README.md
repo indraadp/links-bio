4470b72fadac00570a0469680056fcbee602b8cb
indraadp <indraproject777@gmail.com>
2022-04-04 10:05:22 +0700

    Initial create some files for structure website
    Signed-off-by: indraadp <indraproject777@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+## About the Project
+This is an open source project that serves as a free alternative to the Linktree website with no third party credits.
+
+Just fork and edit this repository then use Github Pages to deploy it.
---
+++ assets/config.js
@@ -0,0 +1,37 @@
+let config = {
+    "Title": "Indra DP - Links Bio",
+    "Name": "Indra DP",
+    "Description": "Hello, I'm a Blogger and Android Enthusiast. Visit more my links 😄👇👍",
+    "links": [
+      {
+        "Title": "Review Storytel",
+        "URL": "https://www.indradp.com/2022/03/review-aplikasi-storytel.html",
+        "icon_classes": "fas fa-rss",
+      },
+      {
+        "Title": "Review blu by BCA Digital",
+        "URL": "https://www.indradp.com/2022/03/review-aplikasi-blu-by-bca-digital.html",
+        "icon_classes": "fas fa-rss",
+      },
+          {
+        "Title": "&nbsp;&nbsp;Indra&#39;s Blog | Blog Berbagi Informasi Teknologi&nbsp;&nbsp;",
+        "URL": "https://www.indradp.com/",
+        "icon_classes": "fab fa-blogger"
+      },
+        {
+        "Title": "&nbsp;&nbsp;Partnership Content Placement&nbsp;&nbsp;",
+        "URL": "https://www.indradp.com/p/kerja-sama-indras-blog.html",
+        "icon_classes": "fas fa-handshake"
+      },
+      {
+        "Title": "Contact Me via Email",
+        "URL": "mailto:indradp.blogger@gmail.com",
+        "icon_classes": "fas fa-envelope"
+      },
+      {
+        "Title": "Contact Me via WhatsApp",
+        "URL": "https://wa.me/628974738855",
+        "icon_classes": "fab fa-whatsapp"
+      },
+    ]
+  }
